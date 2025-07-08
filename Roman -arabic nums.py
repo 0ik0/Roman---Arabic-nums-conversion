4119e3d138ab8d44413ef698b9fefc1e9a0a581d
0ik0 <ikibekwe3@gmail.com>
2025-07-07 21:06:04 -0500

    Update Roman -arabic nums.py

--- Roman -arabic nums.py
+++ Roman -arabic nums.py
@@ -1,17 +1,14 @@
 dict = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
-y = input('Enter value: ')
+y = input('Enter value (capital letters only): ')
 x = list(y)
 
 m = 0
 for i in range(len(x) - 1):
-    
     if dict[x[i]] >= dict[x[i + 1]]:
         m += dict[x[i]]
-
     else:    
         m = m - dict[x[i]]
-    
-    
+        
 m += dict[x[len(x) - 1]]
 
 print(m)
